75790e3fc2434a75a66ee6bb90eea85a72259647
Alexsandr Semeov <an.semionov@gmail.com>
2018-08-10 22:26:34 +0300

    Выделение метода GetValueOrDefault

---
+++ Dodo.Core/Common/XElementEtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Dodo.Core.Common
+{
+    public static class XElementEtensions
+    {
+        public static String GetValueOrDefault(this XElement element, String atributName, String defaultValue)
+        {
+            var attributeValue = element.Attribute(atributName);
+            return attributeValue == null ? defaultValue : attributeValue.Value;
+        }
+    }
+}
