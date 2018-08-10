273c650331c7457ee2c4a5f489582069bc0d7af7
Alexsandr Semeov <an.semionov@gmail.com>
2018-08-10 22:20:29 +0300

    LocalizedContent замена явных типов данных на тип var

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/LocalizedContext.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/LocalizedContext.cs
@@ -13,16 +13,16 @@ namespace Dodo.RestaurantBoard.Site.Controllers {
         public static string LocalizedContent(IHostingEnvironment hostingEnvironment, string contentPath)
         {
             if (string.IsNullOrEmpty(contentPath))
-                throw new ArgumentNullException(nameof (contentPath));
-            StringBuilder stringBuilder = new StringBuilder();
-            CultureInfo currentUiCulture = Thread.CurrentThread.CurrentUICulture;
-            int serverPathLength = hostingEnvironment.WebRootPath.Length - 1;
+                throw new ArgumentNullException(nameof(contentPath));
+            var stringBuilder = new StringBuilder();
+            var currentUiCulture = Thread.CurrentThread.CurrentUICulture;
+            var serverPathLength = hostingEnvironment.WebRootPath.Length - 1;
             contentPath = contentPath.Trim('/');
-            string path1 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", currentUiCulture.TwoLetterISOLanguageName, contentPath);
+            var path1 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", currentUiCulture.TwoLetterISOLanguageName, contentPath);
             if (File.Exists(path1))
                 return ConvertLocalPathToRelativeUrl(path1, serverPathLength);
             stringBuilder.AppendLine(path1);
-            string path2 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", contentPath);
+            var path2 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", contentPath);
             if (File.Exists(path2))
                 return ConvertLocalPathToRelativeUrl(path2, serverPathLength);
             stringBuilder.AppendLine(path2);
