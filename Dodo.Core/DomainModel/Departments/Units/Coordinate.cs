68376c176c50f8068e03c26d8d4731f2925cde36
Alexsandr Semeov <an.semionov@gmail.com>
2018-08-10 22:23:39 +0300

    Выделение класса Coordinate

---
+++ Dodo.Core/DomainModel/Departments/Units/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dodo.Core.DomainModel.Departments.Units
+{
+    public class Coordinate
+    {
+        public Coordinate(decimal x, decimal y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Decimal? X { get; private set; }
+        public Decimal? Y { get; private set; }
+    }
+}
--- Dodo.Core/DomainModel/Departments/Units/Pizzeria.cs
+++ Dodo.Core/DomainModel/Departments/Units/Pizzeria.cs
@@ -4,62 +4,60 @@ using Dodo.Core.DomainModel.Management.Organizations;
 
 namespace Dodo.Core.DomainModel.Departments.Units
 {
-	[Serializable]
-	public class Pizzeria : Unit
-	{
-		public PizzeriaFormat Format { get; private set; }
-		public Double Square { get; private set; }
-		public DateTime? BeginDateTimeWork { get; private set; }
-		public String Orientation { get; private set; }
-		public Boolean? CardPaymentPickup { get; private set; }
-		public Decimal? CoordinateX { get; private set; }
-		public Decimal? CoordinateY { get; private set; }
-		public ClientTreatment ClientTreatment { get; private set; }
+    [Serializable]
+    public class Pizzeria : Unit
+    {
+        public PizzeriaFormat Format { get; private set; }
+        public Double Square { get; private set; }
+        public DateTime? BeginDateTimeWork { get; private set; }
+        public String Orientation { get; private set; }
+        public Boolean? CardPaymentPickup { get; private set; }
+        public Coordinate Coordinate { get; set; }
+        public ClientTreatment ClientTreatment { get; private set; }
         public Boolean TerminalAtCourier { get; private set; }
 
-		public Boolean IsExistsPizzeriaFormat { get { return Format != null; }}
+        public Boolean IsExistsPizzeriaFormat { get { return Format != null; } }
 
-		public Pizzeria(Int32 id, Uuid uuid, String name, String alias, String translitAlias, UnitApprove approve, UnitState state, 
+        public Pizzeria(Int32 id, Uuid uuid, String name, String alias, String translitAlias, UnitApprove approve, UnitState state,
             Int32 departmentId, Uuid departmentUuid, Int32 countryId, OrganizationShortInfo organization, Double square,
-			DateTime? beginDateTimeWork, String orientation, Boolean? cardPaymentPickup, Decimal? coordinateX, Decimal? coordinateY,
-			ClientTreatment clientTreatment, Boolean terminalAtCourier, PizzeriaFormat pizzeriaFormat)
-			: base(id, uuid, name, alias, UnitType.Pizzeria, state, departmentId, departmentUuid, countryId, organization)
-		{
-			Format = pizzeriaFormat;
-			Square = square;
-			BeginDateTimeWork = beginDateTimeWork;
-			Orientation = orientation;
-			CardPaymentPickup = cardPaymentPickup;
-			CoordinateX = coordinateX;
-			CoordinateY = coordinateY;
-			ClientTreatment = clientTreatment;
+            DateTime? beginDateTimeWork, String orientation, Boolean? cardPaymentPickup, Coordinate coordinate,
+            ClientTreatment clientTreatment, Boolean terminalAtCourier, PizzeriaFormat pizzeriaFormat)
+            : base(id, uuid, name, alias, UnitType.Pizzeria, state, departmentId, departmentUuid, countryId, organization)
+        {
+            Format = pizzeriaFormat;
+            Square = square;
+            BeginDateTimeWork = beginDateTimeWork;
+            Orientation = orientation;
+            CardPaymentPickup = cardPaymentPickup;
+            Coordinate = coordinate;
+            ClientTreatment = clientTreatment;
 
-			Approve = approve;
-			TranslitAlias = translitAlias;
+            Approve = approve;
+            TranslitAlias = translitAlias;
             TerminalAtCourier = terminalAtCourier;
 
         }
 
-		public Int32 GetYearsOld(DateTime currentDateTime)
-		{
-			if (!BeginDateTimeWork.HasValue) return 0;
+        public Int32 GetYearsOld(DateTime currentDateTime)
+        {
+            if (!BeginDateTimeWork.HasValue) return 0;
 
-			DateTime zeroDate = new DateTime(1,1,1);
-			TimeSpan diffDate = currentDateTime.Subtract(BeginDateTimeWork.Value);
+            DateTime zeroDate = new DateTime(1, 1, 1);
+            TimeSpan diffDate = currentDateTime.Subtract(BeginDateTimeWork.Value);
 
-			return (zeroDate + diffDate).Year;
-		}
+            return (zeroDate + diffDate).Year;
+        }
 
-		public Int32 GetMonthsOld(DateTime currentDateTime)
-		{
-			const Int32 monthInYear = 12;
+        public Int32 GetMonthsOld(DateTime currentDateTime)
+        {
+            const Int32 monthInYear = 12;
 
-			if (!BeginDateTimeWork.HasValue) return 0;
+            if (!BeginDateTimeWork.HasValue) return 0;
 
-			DateTime zeroDate = new DateTime(1, 1, 1);
-			TimeSpan diffDate = currentDateTime.Subtract(BeginDateTimeWork.Value);
+            DateTime zeroDate = new DateTime(1, 1, 1);
+            TimeSpan diffDate = currentDateTime.Subtract(BeginDateTimeWork.Value);
 
-			return (zeroDate + diffDate).Month - 1 + (monthInYear * GetYearsOld(currentDateTime));
-		}
-	}
+            return (zeroDate + diffDate).Month - 1 + (monthInYear * GetYearsOld(currentDateTime));
+        }
+    }
 }
